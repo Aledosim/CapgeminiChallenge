18b9dd7411be6855bfdf627e88587d4889481acf
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-17 21:08:55 -0300

    Create initial project for Question1 based on Java 11 + JUnit 5 Quick Start Maven Archetype (https://github.com/deangerber/java11-junit5-archetype)

---
+++ question1/src/main/java/org/challenge/question1/Question1.java
@@ -0,0 +1,8 @@
+package org.challenge.question1;
+
+public class Question1 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ question1/src/test/java/org/challenge/question1/Question1Should.java
@@ -0,0 +1,12 @@
+package org.challenge.question1;
+
+import org.junit.jupiter.api.Test;
+
+class Question1Integration {
+
+  // Question1 should print a message
+  @Test
+  void print_a_message() {
+
+  }
+}
