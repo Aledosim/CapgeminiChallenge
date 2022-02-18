2238ea4aaafd4968417633940a230c6e6f5f2f60
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-18 01:37:47 -0300

    Creates first version of end-to-end test for happy path

--- question1/src/test/java/org/challenge/question1/Question1Should.java
+++ question1/src/test/java/org/challenge/question1/Question1Should.java
@@ -2,11 +2,38 @@ package org.challenge.question1;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class Question1Integration {
 
-  // Question1 should print a message
+  // Question1 should print a message and exit without errors
   @Test
-  void print_a_message() {
+  void print_a_message() throws IOException, InterruptedException {
+    // Build the Question1 subprocess
+    ProcessBuilder builder = JavaProcess.exec(Question1.class);
 
+    // Starts the subprocess and wait for it fo finish
+    Process process = builder.start();
+    process.waitFor();
+
+    // Read the stdout of the subprocess and writes it in a List
+    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    List<String> output = new ArrayList<>();
+    String line;
+    while ((line = in.readLine()) != null) {
+      output.add(line);
+    }
+    in.close();
+
+    assertFalse(output.isEmpty());
+    assertEquals(0, process.exitValue());
   }
+
 }
