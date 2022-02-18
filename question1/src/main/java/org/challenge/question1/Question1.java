672796d89093789f93fa6fbbbe0bb2f2fa1f2b06
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-18 15:08:41 -0300

    Refactors Question1 to print stairs and exit without errors

--- question1/src/main/java/org/challenge/question1/Question1.java
+++ question1/src/main/java/org/challenge/question1/Question1.java
@@ -2,8 +2,11 @@ package org.challenge.question1;
 
 public class Question1 {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws Exception {
+    int n = Integer.parseInt(args[0]);
+    for (int i = 1; i < n+1; i++) {
+      print_line(n, i);
+    }
   }
 
   static void print_line(int line_length, int line_fill) throws Exception {
