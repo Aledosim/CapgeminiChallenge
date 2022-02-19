5093c00189fc010511cb5f7730cbb72bcd7ccbe4
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-19 10:46:04 -0300

    Create initial project for Question3 based on same archetype as Question1

---
+++ question3/src/test/java/org/challenge/question3/Question3Should.java
@@ -0,0 +1,12 @@
+package org.challenge.question3;
+
+import org.junit.jupiter.api.Test;
+
+class Question3Integration {
+
+  // Question3 should print a message
+  @Test
+  void print_a_message() {
+
+  }
+}
