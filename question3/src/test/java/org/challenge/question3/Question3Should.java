468b9f25b936987faf9432397c191e1fe040fdc2
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-19 11:31:35 -0300

    Adapts Question1's first version of end-to-end test to Question3

--- question3/src/test/java/org/challenge/question3/Question3Should.java
+++ question3/src/test/java/org/challenge/question3/Question3Should.java
@@ -1,12 +1,39 @@
 package org.challenge.question3;
 
+import org.challenge.utils.JavaProcess;
 import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class Question3Integration {
 
-  // Question3 should print a message
+  // Question3 should print a message and exit without errors
   @Test
-  void print_a_message() {
+  void print_a_message() throws IOException, InterruptedException {
+    // Build the Question1 subprocess
+    ProcessBuilder builder = JavaProcess.exec(Question3.class);
 
+    // Starts the subprocess and wait for it fo finish
+    Process process = builder.start();
+    process.waitFor();
+
+    // Read the stdout of the subprocess and writes it in a List
+    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    List<String> output = new ArrayList<>();
+    String line;
+    while ((line = in.readLine()) != null) {
+      output.add(line);
+    }
+    in.close();
+
+    assertFalse(output.isEmpty());
+    assertEquals(0, process.exitValue());
   }
 }
