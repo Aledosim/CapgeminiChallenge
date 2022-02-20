34bd65e099cab34f30e25a108a05cbd874880abe
Alexandre do Sim <aledosim@yahoo.com.br>
2022-02-20 00:22:22 -0300

    Create initial project based on same archetype as Question1 and the changes of Question3, including the first version of integration test

---
+++ question2/src/main/java/org/challenge/question2/Question2.java
@@ -0,0 +1,8 @@
+package org.challenge.question2;
+
+public class Question2 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ question2/src/test/java/org/challenge/question2/Question2Integration.java
@@ -0,0 +1,39 @@
+package org.challenge.question2;
+
+import org.challenge.utils.JavaProcess;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class Question2Integration {
+
+  // Question2 should print a message and exit without errors
+  @Test
+  void print_a_message() throws IOException, InterruptedException {
+    // Build the Question1 subprocess
+    ProcessBuilder builder = JavaProcess.exec(Question2.class);
+
+    // Starts the subprocess and wait for it fo finish
+    Process process = builder.start();
+    process.waitFor();
+
+    // Read the stdout of the subprocess and writes it in a List
+    BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    List<String> output = new ArrayList<>();
+    String line;
+    while ((line = in.readLine()) != null) {
+      output.add(line);
+    }
+    in.close();
+
+    assertFalse(output.isEmpty());
+    assertEquals(0, process.exitValue());
+  }
+}
